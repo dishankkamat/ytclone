5f457a4523a702589d00da70fe29352f0f41450c
Your Name <you@example.com>
2024-08-27 15:39:36 +0530

    btnList && VideoCard && VideoContainer && API of yt

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Button from "./Button";
 
 const ButtonList = () => {
-  return <div>ButtonList</div>;
+  return (
+    <div className="flex">
+      <Button name="All" />
+      <Button name="Live" />
+      <Button name="Movies" />
+      <Button name="Cricket" />
+      <Button name="Drama" />
+      <Button name="Comedy" />
+      <Button name="Football" />
+      <Button name="Songs" />
+      <Button name="Gaming" />
+      <Button name="Fun" />
+      <Button name="Coding" />
+      <Button name="Web Dev" />
+      <Button name="Films" />
+    </div>
+  );
 };
 
 export default ButtonList;
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+const GOOGLE_API_KEY = "AIzaSyCyR5bTQGtdnNbGH0MshWtXE09se5fwsL4";
+export const YOUTUBE_VIDEO_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  GOOGLE_API_KEY;
